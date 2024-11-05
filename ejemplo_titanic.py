9cbae3c44837f1b76bb4e7997b35c06f42f285f8
FELIP4XO <felipeorostica15@gmail.com>
2024-11-05 20:46:38 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -12,11 +12,14 @@ ax.scatter(datos_sismicos['Magnitude'], datos_sismicos['Depth'])
 ax.set_xlabel('Magnitud')
 ax.set_ylabel('Profundidad')
 ax.set_title('Profundidad vs Magnitud')
-
 st.pyplot(fig)
 
-columnas_numericas = datos_sismicos.select_dtypes(include=['number']).columns
+columnas_numericas = datos_sismicos.select_dtypes(include=['number']).columns[-2:]
 
+with st.sidebar:
+    st.header('opciones de color')
+    color_grafico = st.color_picker('Selecciona un color para el grafico','#007bff')
+    
 for columna in columnas_numericas:
     if st.button(f'Grafico de barra de {columna}'):
         fig, ax = plt.subplots()
